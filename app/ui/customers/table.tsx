992bc6ba58939739d858ae20f40792fbc335d25f
troll <trino@mail.ru>
2023-11-05 22:19:42 +0300

    Исправил подключение шрифта

--- app/ui/customers/table.tsx
+++ app/ui/customers/table.tsx
@@ -1,5 +1,5 @@
 import Image from 'next/image';
-import { lusitana } from '@/app/ui/fonts';
+import { lusitana } from '../invoices/fonts';
 import Search from '@/app/ui/search';
 import { CustomersTable, FormattedCustomersTable } from '@/app/lib/definitions';
 
